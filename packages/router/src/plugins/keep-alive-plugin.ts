5c8504c11c3655a1a686da31bcc328596f8b8a7e
liyuan <1824581048@qq.com>
2025-07-04 16:17:50 +0800

    refactor(keep-alive-plugin): 更新类型定义，优化缓存逻辑，支持使用 RegExp 作为路由名称

--- packages/router/src/plugins/keep-alive-plugin.ts
+++ packages/router/src/plugins/keep-alive-plugin.ts
@@ -1,89 +1,67 @@
-import type { RouteRecordNameGeneric, RouteRecordRaw } from 'vue-router'
+import type { RouteRecordNameGeneric } from 'vue-router'
 import type { ProRouterPlugin } from '../create-router'
-import { isBoolean, isString } from 'lodash-es'
+import { isBoolean, isSymbol } from 'lodash-es'
 
 declare module 'vue-router' {
   interface RouteMeta {
     keepAlive?: boolean | {
       // 这些路由返回时，不缓存当前页面
-      exclude: string[]
+      exclude: (string | symbol)[]
     }
   }
 }
 
-function extractKeepAliveNames(routes: readonly RouteRecordRaw[]): string[] {
-  const result: string[] = []
-
-  const traverse = (routes: readonly RouteRecordRaw[]) => {
-    for (const route of routes) {
-      if (route.meta?.keepAlive) {
-        if (typeof route.name === 'string') {
-          result.push(route.name)
-        }
-      }
-      if (route.children?.length) {
-        traverse(route.children)
-      }
-    }
-  }
-
-  traverse(routes)
-  return result
-}
-
-interface KeepAliveStoreLike {
-  /**
-   * 初始化缓存列表
-   * @param keepAlive 路由 name 组成的缓存列表
-   */
-  init: (keepAlive: string[]) => void
+type KeepAliveStoreLike = () => ({
   /**
    * 添加一个缓存 name
    * @param name 路由名称（必须为字符串）
    */
-  add: (name: string) => void
+  add: (name: string | RegExp) => void
   /**
    * 缓存列表中移除指定的路由 name
    * @param name 路由名称（必须为字符串）
    */
-  remove: (name: string) => void
+  remove: (name: string | RegExp) => void
 }
-
+)
 interface KeepAlivePluginOptions {
   store: KeepAliveStoreLike
+  /**
+   * @default false
+   */
+  defualtKeepAlive?: boolean
 }
 
-export function keepAlivePlugin(store: KeepAlivePluginOptions): ProRouterPlugin {
-  // 插件初始化阶段，提取全部默认需要缓存的 name
-  const defaultKeepAliveNames = extractKeepAliveNames(routes)
-  store.init(defaultKeepAliveNames)
-  function add(name: RouteRecordNameGeneric) {
-    if (isString(name))
-      store.add(name)
-  }
-  function remove(name: RouteRecordNameGeneric) {
-    if (isString(name))
-      store.remove(name)
-  }
+function normalizeRouteName(name: RouteRecordNameGeneric): string {
+  return isSymbol(name) ? (new RegExp(name.toString())).toString() : name.toString()
+}
 
+export function keepAlivePlugin({
+  store,
+  defualtKeepAlive = false,
+}: KeepAlivePluginOptions): ProRouterPlugin {
   return {
     name: '@pro/router-plugin-keep-alive',
     beforeEach(to, from) {
-      if (!from.name)
-        return
-      const config = from.meta.keepAlive
-      if (isBoolean(config)) {
-        config ? add(from.name) : remove(from.name)
+      if (!from.name) {
         return
       }
-      if (config?.noCacheWhenBackFrom) {
-        const noShouldKeep = config.noCacheWhenBackFrom
-          .map(String)
-          .includes(String(to.name))
-        noShouldKeep ? remove(from.name) : add(from.name)
+      const { add, remove } = store()
+      const { keepAlive = defualtKeepAlive } = from.meta
+      const normalizedName = normalizeRouteName(from.name)
+      if (isBoolean(keepAlive)) {
+        keepAlive ? add(normalizedName) : remove(normalizedName)
         return
       }
-      remove(from.name)
+      if (
+        keepAlive.exclude
+          .map(normalizeRouteName)
+          .includes(normalizeRouteName(to.name))
+      ) {
+        remove(normalizedName)
+        return
+      }
+      add(normalizedName)
     },
   }
 }
--- src/store/use-routes-keep-alive/index.ts
+++ src/store/use-routes-keep-alive/index.ts
@@ -2,18 +2,14 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useRoutesKeepAliveStore = defineStore('routes-keep-alive', () => {
-  const keepAliveList = ref<string[]>([])
+  const keepAliveList = ref<(string | RegExp)[]>([])
 
-  function init(keepAlive: string[]) {
-    keepAliveList.value = keepAlive
-  }
-
-  function add(name: string) {
+  function add(name: string | RegExp) {
     if (!keepAliveList.value.includes(name))
       keepAliveList.value.push(name)
   }
 
-  function remove(name: string) {
+  function remove(name: string | RegExp) {
     keepAliveList.value = keepAliveList.value.filter(n => n !== name)
   }
 
@@ -22,7 +18,6 @@ export const useRoutesKeepAliveStore = defineStore('routes-keep-alive', () => {
   }
 
   return {
-    init,
     add,
     remove,
     getKeepAliveList,
