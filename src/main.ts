8a68cd7710c94ac127f7ab25e5319a5201ace1fb
Zheng-Changfu <zcfpromise@163.com>
2025-07-14 14:40:22 +0800

    chore: 移除不再使用的 nprogress 样式导入

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { setupPinia } from '@/store'
 import App from './App.vue'
 import { setupRouter } from './router'
-import 'nprogress/nprogress.css'
 import 'virtual:uno.css'
 import './assets/reset.css'
 
