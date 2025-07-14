2112a10dacd1c3fc8c42762deba1ff5db181388f
Zheng-Changfu <zcfpromise@163.com>
2025-07-14 17:31:03 +0800

    fix(router): update document title plugin to handle undefined titleTemplate and use symbol names

--- packages/router/src/plugins/document-title-plugin.ts
+++ packages/router/src/plugins/document-title-plugin.ts
@@ -1,7 +1,7 @@
 import type { NavigationFailure, RouteLocationNormalized } from 'vue-router'
 import type { ProRouterPlugin } from '../plugin'
 import { useTitle } from '@vueuse/core'
-import { isString } from 'lodash-es'
+import { isSymbol } from 'lodash-es'
 
 declare module 'vue-router' {
   interface RouteMeta {
@@ -10,17 +10,18 @@ declare module 'vue-router' {
 }
 
 interface DocumentTitlePluginOptions {
-  titleTemplate:
-    | string
-    | ((to: RouteLocationNormalized, from: RouteLocationNormalized, failure?: NavigationFailure | void) => string)
+  titleTemplate?: ((to: RouteLocationNormalized, from: RouteLocationNormalized, failure?: NavigationFailure | void) => string)
 }
 
-export function documentTitlePlugin({ titleTemplate }: DocumentTitlePluginOptions): ProRouterPlugin {
+export function documentTitlePlugin({ titleTemplate }: DocumentTitlePluginOptions = {}): ProRouterPlugin {
   return ({ router }) => {
     router.afterEach((to, from, failure) => {
-      useTitle(to.meta.title, {
+      const title = to.meta.title ?? (isSymbol(to.name) ? to.name.toString() : to.name)
+      useTitle(title, {
         titleTemplate: () => {
-          return isString(titleTemplate) ? titleTemplate : titleTemplate(to, from, failure)
+          return titleTemplate
+            ? titleTemplate(to, from, failure)
+            : title
         },
       })
     })
