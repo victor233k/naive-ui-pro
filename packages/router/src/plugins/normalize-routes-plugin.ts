8cc52e0571d0643982295db3f60d22f1961c9c08
Zheng-Changfu <zcfpromise@163.com>
2025-07-14 17:30:38 +0800

    feat(router): 增加标准化路由插件

--- packages/router/src/plugins/normalize-routes-plugin.ts
+++ packages/router/src/plugins/normalize-routes-plugin.ts
@@ -1,12 +1,15 @@
-import type { RouteLocationNormalizedGeneric } from 'vue-router'
+import type { RouteLocationNormalizedGeneric, Router } from 'vue-router'
 import type { ProRouterPlugin } from '../plugin'
 import { isString, isSymbol } from 'lodash-es'
 
 export function normalizeRoutesPlugin(): ProRouterPlugin {
   return ({ router }) => {
+    router.beforeResolve((to) => {
+      return tryRedirectToFirstChild(router, to)
+    })
+
     router.afterEach((to) => {
       tryUpdateComponentName(to)
-      tryUpdateRouteRedirect(to)
     })
   }
 }
@@ -15,7 +18,6 @@ function tryUpdateComponentName(to: RouteLocationNormalizedGeneric) {
   const currentRoute = to.matched[to.matched.length - 1]
   const currentRouteName = currentRoute.name
   const currentRouteComponentName = currentRoute.components.default.name
-
   if (__DEV__) {
     if (
       isString(currentRouteName)
@@ -47,29 +49,18 @@ function tryUpdateComponentName(to: RouteLocationNormalizedGeneric) {
   }
 }
 
-function tryUpdateRouteRedirect(to: RouteLocationNormalizedGeneric) {
-  // const currentRoute = to.matched[to.matched.length - 1]
-  // const currentRouteRedirect = currentRoute.redirect
-
-  // if (currentRouteRedirect) {
-  //   currentRoute.redirect = currentRouteRedirect
-  // }
+function tryRedirectToFirstChild(router: Router, to: RouteLocationNormalizedGeneric) {
+  const currentRoute = to.matched[to.matched.length - 1]
+  if (
+    !currentRoute.redirect
+    && currentRoute.children
+    && currentRoute.children.length > 0
+  ) {
+    const firstChildRoute = currentRoute.children[0]
+    const resolved = router.resolve(firstChildRoute)
+    return {
+      ...resolved,
+      replace: true,
+    }
+  }
 }
-
-/**
- * createRouter({
- *  ...
- *  plugin:[
- *    normalizePlugin() // 标准化路由插件
-      keepAlivePlugin() // 路由缓存插件
-      documentTitlePlugin() // 设置 title 插件
-      breadcrumbPlugin() // 面包屑插件，将当前路由结构化成面包屑数据
-      progressPlugin() // 进度条插件
-      rbacAccessPlugin() // 权限插件
-      linkPlugin() // 路由跳转插件，支持外链跳转
-      menusPlugin() // 菜单插件，将路由表转换成菜单数据
-      transitionPlugin() // 路由过渡插件
-      tabsPlugin() // 标签页插件
- *  ]
- * })
- */
