b6cbab680950b85142dcab564ad59484131cf76d
Zheng-Changfu <zcfpromise@163.com>
2025-07-21 19:04:28 +0800

    chore: 完善 logo 组件

--- src/components/layout/logo.vue
+++ src/components/layout/logo.vue
@@ -1,16 +1,51 @@
 <script setup lang='tsx'>
+import type { ProLayoutMode } from 'pro-naive-ui'
 import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
 import logo from '@/assets/logo.svg'
 import { useAppStore } from '@/store/use-app-store'
 import { useLayoutStore } from '@/store/use-layout-store'
 
 const { title } = storeToRefs(useAppStore())
-const { mobile, collapsed } = storeToRefs(useLayoutStore())
+const {
+  mode,
+  mobile,
+  collapsed,
+} = storeToRefs(useLayoutStore())
+
+const enablePaddingLeft = computed(() => {
+  const layoutMode = mode.value as ProLayoutMode
+  if (mobile.value) {
+    return true
+  }
+  return layoutMode === 'horizontal'
+    || layoutMode === 'sidebar'
+    || layoutMode === 'mixed-sidebar'
+    || (layoutMode === 'vertical' && !collapsed.value)
+})
+
+const showAppTitle = computed(() => {
+  const layoutMode = mode.value as ProLayoutMode
+  if (mobile.value) {
+    return false
+  }
+  return layoutMode === 'sidebar'
+    || layoutMode === 'horizontal'
+    || layoutMode === 'mixed-sidebar'
+    || (layoutMode === 'vertical' && !collapsed.value)
+})
 </script>
 
 <template>
-  <a href="javascript: void 0;" class="flex items-center gap-12px h-full pl-12px">
+  <a
+    href="javascript: void 0;"
+    class="flex items-center gap-12px h-full"
+    :class="{
+      'pl-12px': enablePaddingLeft,
+      'justify-center': !enablePaddingLeft,
+    }"
+  >
     <img :src="logo" class="size-32px truncate">
-    <h1 v-show="!mobile && !collapsed" class="truncate text-18px">{{ title }}</h1>
+    <h1 v-show="showAppTitle" class="truncate text-18px">{{ title }}</h1>
   </a>
 </template>
