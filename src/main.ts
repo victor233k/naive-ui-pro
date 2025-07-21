4fc29482c55832e073828495cdb7b8190400893d
Zheng-Changfu <zcfpromise@163.com>
2025-07-21 19:05:53 +0800

    feat(mock): 生产环境启用 mock

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
+import { createProdMockServer } from 'vite-plugin-mock/client'
 import { createApp } from 'vue'
 import { setupPinia } from '@/store'
+import mockList from '../mock/index'
 import App from './App.vue'
 import { setupRouter } from './router'
 import 'virtual:uno.css'
 import './assets/reset.css'
 
 async function bootstrap() {
+  if (!__DEV__) {
+    createProdMockServer(mockList)
+  }
   const app = createApp(App)
   await setupPinia(app)
   await setupRouter(app)
