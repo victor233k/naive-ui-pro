5de42f3a53edc3c3985733b8517a31c8a92f1b2f
leihaohao <2468048176@qq.com>
2025-08-05 21:37:40 +0800

    refactor(utils): 重构响应拦截器
    - 合并错误拦截器和数据格式化拦截器为一个 setupResponseInterceptors 函数
    - 新增 unwrapResponseData 函数用于拆解服务端响应数据格式
    - 新增 handleError 函数用于全局错误处理
    - 优化了错误处理机制，确保能捕获所有类型的错误
    - 调整了 AxiosResponse 接口，使用 rawData 替代 rawResponse

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { setupTokenInterceptor } from './request-interceptors'
-import { setupErrorInterceptor, setupTransformResponseInterceptor } from './response-interceptors'
+import { setupResponseInterceptors } from './response-interceptors'
 
 const http = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -10,13 +10,47 @@ const http = axios.create({
  * 请求前添加 token 的拦截器
  */
 setupTokenInterceptor(http)
+
 /**
- * 响应错误拦截器
+ * 安装响应拦截器
  */
-setupErrorInterceptor(http)
-/**
- * 请求后数据格式化拦截器
- */
-setupTransformResponseInterceptor(http)
+setupResponseInterceptors(http, {
+  /**
+   * 拆解服务端响应数据格式，成功时返回数据，失败时需要抛出错误消息
+   * @param response axios 响应体
+   * @returns 拆解后的数据
+   * @throws 错误消息
+   * @example
+   * ```ts
+   * (response) => response.data.success ? response.data.data : throw new Error(response.data.message)
+   * ```
+   */
+  unwrapResponseData(response) {
+    if (response.data != null) {
+      // 格式: { code: number, data: any, message: string }
+      if ('code' in response.data) {
+        if (response.data.code !== 200) {
+          throw new Error(response.data.message)
+        }
+        return response.data.data
+      }
+    }
+    return response.data
+  },
+
+  /**
+   * 全局错误处理，该函数的返回体将决定调用侧的 Promise 的状态
+   * @param error axios 响应错误
+   */
+  handleError(error) {
+    // TODO: 需要全局进行错误处理时可以在这里定义
+
+    // 调用侧将会进入 then 回调并获取数据
+    // return Promise.resolve({})
+
+    // 调用侧将会进入 catch 回调，并获取错误对象
+    return Promise.reject(error)
+  },
+})
 
 export default http
--- src/utils/axios/response-interceptors.ts
+++ src/utils/axios/response-interceptors.ts
@@ -1,35 +1,59 @@
 import type { AxiosInstance, AxiosResponse } from 'axios'
-import { get } from 'lodash-es'
+import { AxiosError, isAxiosError } from 'axios'
+import { isError, toString } from 'lodash-es'
 
 declare module 'axios' {
-  interface AxiosResponse<T = any, D = any> {
-    rawResponse: AxiosResponse<T, D>
+  interface AxiosResponse {
+    rawData: any
   }
 }
 
-export function setupTransformResponseInterceptor(http: AxiosInstance) {
-  http.interceptors.response.use((response) => {
-    response.rawResponse = response
-    if (isSuccess(response)) {
-      return response.data
-    }
-    return response
-  })
+export interface SetupResponseInterceptorsOptions {
+  unwrapResponseData: (response: AxiosResponse) => any
+  handleError: (error: AxiosError) => any
 }
 
-export function setupErrorInterceptor(http: AxiosInstance) {
-  http.interceptors.response.use(null, (error) => {
-    return Promise.reject(error)
-  })
-
+export function setupResponseInterceptors(
+  http: AxiosInstance,
+  options: SetupResponseInterceptorsOptions,
+) {
   http.interceptors.response.use((response) => {
-    if (!isSuccess(response)) {
-      return Promise.reject(response)
-    }
+    unwrapResponseData(response, options)
     return response
   })
+
+  // 在这里注册错误拦截，确保能捕获所有的错误
+  http.interceptors.response.use(null, (error) => handleError(error, options))
 }
 
-function isSuccess(response: AxiosResponse) {
-  return get(response, 'data.code') === 200
+function unwrapResponseData(
+  response: AxiosResponse,
+  options: SetupResponseInterceptorsOptions,
+) {
+  try {
+    response.rawData = response.data
+    response.data = options.unwrapResponseData(response)
+  } catch (e: unknown) {
+    throw isAxiosError(e)
+      ? e
+      : isError(e)
+      ? AxiosError.from(
+          e,
+          AxiosError.ERR_BAD_RESPONSE,
+          response.config,
+          response.request,
+          response,
+        )
+      : new AxiosError(
+          toString(e) || 'Unknown Error',
+          AxiosError.ERR_BAD_RESPONSE,
+          response.config,
+          response.request,
+          response,
+        )
+  }
+}
+
+function handleError(error: AxiosError, options: SetupResponseInterceptorsOptions) {
+  return options.handleError(error)
 }
