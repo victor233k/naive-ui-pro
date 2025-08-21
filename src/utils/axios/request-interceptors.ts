409ee5c21df89d369909c49d33349c56e8fd9490
Zheng-Changfu <zcfpromise@163.com>
2025-08-21 21:19:12 +0800

    fix: 解决生产环境 iconify 远程搜索时出现跨域问题 - axios 支持配置 addToken 来决定是否需要添加 token - iconify search api 不添加 token

--- src/utils/axios/request-interceptors.ts
+++ src/utils/axios/request-interceptors.ts
@@ -1,10 +1,22 @@
 import type { AxiosInstance } from 'axios'
 
+declare module 'axios' {
+  interface AxiosRequestConfig {
+    /**
+     * 是否需要携带 token
+     * @default true
+     */
+    addToken?: boolean
+  }
+}
+
 export function setupTokenInterceptor(http: AxiosInstance) {
   http.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+    if (config.addToken !== false) {
+      const token = localStorage.getItem('token')
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+      }
     }
     return config
   })
