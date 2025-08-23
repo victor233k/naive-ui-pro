4eb1d640e0d208b6941de8166a14415a957b39f8
Zheng-Changfu <zcfpromise@163.com>
2025-08-23 23:05:46 +0800

    fix: 解决生产环境 iconify 远程搜索时出现跨域问题 - axios 支持配置 addToken 来决定是否需要添加 token - iconify search api 不添加 token

--- src/components/iconify-icons/index.api.ts
+++ src/components/iconify-icons/index.api.ts
@@ -7,6 +7,7 @@ interface QueryIconsResponse {
 export class Api {
   static queryIcons(query: string, limit: number) {
     return http<QueryIconsResponse>('/search', {
+      addToken: false,
       baseURL: import.meta.env.VITE_ICONIFY_API_PREFIX_URL,
       params: {
         query,
